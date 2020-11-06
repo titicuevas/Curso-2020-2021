3f15b490178ff03d1a81d696c4d7d3e6955bb9ea
Enrique Cuevas Garcia <enrique.cuevas@iesdonana.org>
2020-11-06 15:30:38 +0100

    Actualizacion de ficheros

---
+++ DWES/ambito2.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+function suma (int $x , int $y): void   
+{
+    var_dump ($x);
+    var_dump ($y);
+}
+suma("4","3");
+
---
+++ DWES/prueba1.php
@@ -0,0 +1,6 @@
+<?php if (4 > 3) { ?>
+    El 4 es mayor que el 3.
+<?php } else { ?>
+    El 3 es mayor que el 4 , o igual.
+<?php }
+
