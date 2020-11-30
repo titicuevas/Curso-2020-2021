1dd8b860b3b43e1175e090cb1365e5c9b3ba6f37
Enrique Cuevas Garcia <enrique.cuevas@iesdonana.org>
2020-11-30 19:38:20 +0100

    Filmaffinity

--- DWES/Filaffinity/filmaffinity.sql
+++ DWES/Filaffinity/filmaffinity.sql
@@ -3,13 +3,75 @@ CREATE TABLE titulos (
     id bigserial PRIMARY KEY,
     titulo_id varchar (255) NOT NULL
 );
+
+DROP TABLE personas CASCADE;
+
+CREATE TABLE personas 
+(
+    id bigint PRIMARY KEY
+    , persona varchar (255) NOT NULL
+
+
+
+);
+
+
+
+DROP TABLE paises CASCADE;
+
+CREATE TABLE paises
+(
+    id bigserial PRIMARY KEY
+    , pais varchar(255) NOT NULL
+
+);
+
+
+
+
+
 DROP TABLE fichas CASCADE;
 CREATE TABLE fichas (id bigserial PRIMARY KEY);
 DROP TABLE fichas_titulos CASCADE;
 CREATE TABLE fichas_titulos (
     fichas_id bigint NOT NULL REFERENCES ficha(id),
     titulo_id bigint NOT NULL REFERENCES titulo(id),
-    titulo_original_id bool,
-    titulo_destacado_id bool,
+    , anyo    numeric(4)
+    , duracion int
+    ,
     PRIMARY KEY (ficha_id, titulo_id)
 );
+
+
+DROP TABLE reparto CASCADE;
+
+CREATE TABLE reparto
+
+(
+    ficha_id bigint NOT NULL REFERENCES ficha (id)
+    , persona_id bigint NOT NULL REFERENCES personas (id)
+
+
+);
+
+
+
+CREATE VIEW fichas_titulos AS
+SELECT id, titulo_destacados_id
+FROM fichas
+UNION
+
+SELECT
+
+FROM fichas f JOIN resto_titulos rt
+    ON f.id = rt.ficha_id
+WHERE f.id = 24;
+
+
+
+
+
+    id ficha  id titulo
+
+
+    -------   ---------
