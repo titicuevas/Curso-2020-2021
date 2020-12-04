54052ec6ca5fd9c945328cfce97d1f138ddf7229
Enrique Cuevas Garcia <enrique.cuevas@iesdonana.org>
2020-12-04 18:02:25 +0100

    Ejemplo.php

---
+++ DWES/Preexamen/ejemplo.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method=post action="ejemplo.php"></form>
+    Calendario : <input type="date" name="calendario">
+</form>
+</body>
+
+</html>
